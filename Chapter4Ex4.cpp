436f5c33356554f7c34d1b673775f42970d5a19c
richards06 <Joshua.l.richards1@gmail.com>
2024-09-20 11:38:02 -0400

    added "switch" / added "cases" / added "breaks"

--- Chapter4Ex4.cpp
+++ Chapter4Ex4.cpp
@@ -2,7 +2,7 @@
    File Name : Chapter4Ex4
    Programmer : Josh Richards
    Date : Sep 20, 2024
-
+   Requirements : 
 
 
 */
@@ -15,6 +15,19 @@ int main()
     int numberBooks;
     std::cout << "How many boods did you purchase?";
     cin >> numberBooks;
+    switch (numberBooks)
+    {
+    case 0: cout << "You have earned NO points" << endl;
+        break;
+    case 1: cout << "You have earned 5 points" << endl;
+        break;
+    case 2: cout << "You have earned 10 points" << endl;
+        break;
+    case 3: cout << "You have earned 30 points" << endl;
+        break;
+    default: "You have earned 60 points" << endl;
+
+    }
     if (numberBooks >= 0)
     {
         if (numberBooks == 0)
